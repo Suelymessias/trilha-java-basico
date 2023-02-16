e72cac456388eb1ebfa08d7ce41f0184d80f903c
Suelymessias <su-2105@hotmail.com>
2023-02-16 15:30:42 -0300

    Pratica variaveis java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,17 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
-        String meuNome = "Suely Messias";
-
-        double salarioMinimo = 2500;
-
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
-
-        int numero = 5;
-        
+           
+              // Declarando e atribuindo valores a variáveis de tipos primitivos
+              int idade = 30;
+              double altura = 1.75;
+              char genero = 'M';
+              boolean casado = true;
+              
+              // Imprimindo os valores das variáveis na tela
+              System.out.println("Idade: " + idade);
+              System.out.println("Altura: " + altura);
+              System.out.println("Gênero: " + genero);
+              System.out.println("Casado: " + casado);
+              
+              // Realizando operações com variáveis de tipos primitivos
+              double imc = 80 / (altura * altura); // Cálculo do índice de massa corporal
+              boolean temMaisDe40Anos = idade > 40; // Verificando se a idade é maior que 40
+              
+              // Imprimindo os resultados das operações na tela
+              System.out.println("IMC: " + imc);
+              System.out.println("Tem mais de 40 anos? " + temMaisDe40Anos);
+            }
+    
+          
 
 
     }
-}
+
